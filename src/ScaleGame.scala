dbd21041ae7d6436bd7efd1ea54f686eca96433f
pekka <pekka@DESKTOP-JM7IUIO>
2019-03-31 21:52:23 +0300

    added comment for playerAction

--- src/ScaleGame.scala
+++ src/ScaleGame.scala
@@ -18,6 +18,9 @@ class ScaleGame (players: Vector[Player]) {
   //"basic scale" for adding just radius 3 scales to the game
   def addscale (loc: Location) = scales :+ new Scale(3, loc)
   
+  
+  //the basic player action, which is adding a weight to a slot on a scale. Might add error handling for clicking on a
+  //scale. Still need to add something, forgot what.
   def playerAction(loc: Location, player: Player) = {
     val scale = loc.getScaleAttachedTo.get
     val slot = loc.getNum
